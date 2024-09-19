53c8c947a6c60799ecaefb68c5c239b645e963fd
Brian Snook <1421890+bmsnook@users.noreply.github.com>
2024-09-18 21:48:52 -0400

    specifying AWS profile

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "The AWS region your resources will be deployed"
 }
+
+variable "aws_profile" {
+  type    = string
+  default = "default"
+}
